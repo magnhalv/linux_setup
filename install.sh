bab73c2b74efadccdc85045e62555633761e3abf
Magnus Halvorsen <magnus.halvorsen@kdi.kongsberg.com>
2021-08-10 12:30:13 +0200

    Add basic linux setup files

---
+++ install.sh
@@ -0,0 +1,6 @@
+# fd - simpler find file
+sudo apt install fd-find
+
+# Ripgrep - better grep
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb
+sudo dpkg -i ripgrep_12.1.1_amd64.deb
