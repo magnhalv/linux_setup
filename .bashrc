74995e9aa8df89ac54e3593416df139e9f8ec722
Magnus Halvorsen <magnus.halvorsen@kdi.kongsberg.com>
2021-09-07 19:36:49 +0200

    Add zoxide

--- .bashrc
+++ .bashrc
@@ -706,3 +706,5 @@ source "$HOME/.cargo/env"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+eval "$(zoxide init bash)"
--- install.sh
+++ install.sh
@@ -4,3 +4,9 @@ sudo apt install fd-find
 # Ripgrep - better grep
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb
 sudo dpkg -i ripgrep_12.1.1_amd64.deb
+
+#fzf - fuzzy finder, used by zoxide
+sudo apt-get install fzf
+
+# zoxide - better cd
+apt install zoxide
